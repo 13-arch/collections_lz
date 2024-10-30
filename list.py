74e1aa4d5372d3698530c44ff05f4677be35e725
13-arch <Iron_Mike@tutamail.com>
2024-10-30 20:53:18 +0300

    laba%5

--- list.py
+++ list.py
@@ -1,22 +1,34 @@
 input_data = open('input.txt', 'r')
 output = open('output.txt', 'w')
 data = input_data.read()
-data = data.split()#разделение элементов
-output.write(str('0, '))#вводим ноль запятую пробел потому что фибоначи начинается с нуля
-a =int(data[0])#выводи число с файла
-f1=0
-f2=1
-for i in range(1,a):#промежуток
-    output.write(str(f1+f2) +(', '))
-    if i%2 ==0 :#условие
+a = int(data)
+list = []
+list.append(0) #добавлем первый ноль
+list.append(1) #добавлем первую единицу
+f1 = 0
+f2 = 1
+k = 0 #количество элементов больше медианного значения
+for i in range(1, a-1):
+
+    if i%2 == 0: #если i четное, то мы обноляем s1
         f1=f1+f2
-    elif i%2==1:#условие
+    elif i%2 == 1: #если i нечетное, то мы обноляем s2(чередуем после каждого числа)
         f2=f1+f2
-if (f2+f1)%2==0:
-        a=(f2+f1)*2
-        output.write(str(a)) 
-elif (f2+f1)%2==1:
-    q =(f2+f1)**2
-    output.write(str(q))      
+    list.append(f1+f2)
+i = 0
+for i in range(0, len(list)): 
+    if list[i] % 2 == 0: #проверяем на четность
+        list[i] = list[i] * 2 #если четное, то умножаем на 2
+    else:
+        list[i] = list[i] ** 2 #если нечетное, то возводим в квадрат
+i = 0
+minel = min(list) #минмимальный элемент
+maxel= max(list) #максимальный элемент
+lenl = len(list) #длина списка
+arifm = sum(list)/len(list) #медианное значение
+for i in range(0, len(list)):
+    if int(list[i]) > arifm:
+        k += 1 #проверяем, сколько чисел бедет больше мидианного значения
+output.write(str('min - ') + str(minel) + str('\nmax - ') + str(maxel) + str('\nlen - ') + str(lenl) + str('\n> median - ') + str(k))
 input_data.close()
 output.close()
